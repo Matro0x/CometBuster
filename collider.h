a7c5a8ad58684f06a65dfa9d6c22478d09415301
Matr0x <115739665+Matro0x@users.noreply.github.com>
2025-11-10 13:19:23 +0000

    chore: cleanup

--- collider.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLLIDER_H
-#define COLLIDER_H
-
-#include <stdbool.h>
-#include "sprite.h"
-
-bool collide_test(sprite_t sprite1, sprite_t sprite2, SDL_PixelFormat* format, int * cu, int * cv);
-
-#endif /* COLLIDER_H */
