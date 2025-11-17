dfc48b4837bd71f1b7c36bb3254d471b5abf7f55
Luis Pic <luispic7@gmail.com>
2025-11-17 15:42:18 +0000

    luis

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -15,20 +15,43 @@ list_ptr list_new(void)
  * */
 list_ptr list_add(sprite_t sprite, list_ptr list)
 {
-  return NULL;
+  list_ptr new_node = (list_ptr)malloc(sizeof(s_list_node_t));
+  if(!new_node){
+    printf("error:Cannot allocate memory for new list node\n");
+    return list;
+  }
+  new_node -> data = sprite;
+  new_node -> next = list;
+  return new_node;
 }
 
 /* Return the length of a list
  * */
 int list_length(list_ptr l)
 {
-  return 0;
+  int count = 0;
+  list_ptr current = l;
+  while (current != NULL) {
+    count++;
+    current = current->next;
+  }
+  return count;
 }
 
 /* Reverse the order of a list
  * */
 void list_reverse(list_ptr * l)
 {
+  list_ptr prev = NULL;
+  list_ptr current = *l;
+  list_ptr next = NULL;
+  while (current != NULL) {
+    next = current->next;
+    current->next = prev;
+    prev = current;
+    current = next;
+  }
+  *l = prev;
 }
 
 /* Copy a list to another one. 
@@ -36,14 +59,35 @@ void list_reverse(list_ptr * l)
  * */
 list_ptr list_clone(list_ptr list)
 {
-  return NULL;
+  if (list == NULL) return NULL; 
+  list_ptr new_list = NULL;
+  list_ptr current = list;
+  list_ptr *last_ptr = &new_list;
+   while (current != NULL) {
+    list_ptr new_node = (list_ptr)malloc(sizeof(s_list_node_t));
+    if (!new_node) {
+      list_ptr temp = new_list;
+      while (temp != NULL) {
+        list_ptr next = temp->next;
+        free(temp);
+        temp = next;
+      }
+      return NULL;
+    }
+    new_node->data = current->data;  
+    new_node->next = NULL;
+    *last_ptr = new_node;
+    last_ptr = &(new_node->next);
+    current = current->next;
+  }
+  return new_list;
 }
 
 /* Return true if the list is empty
  * */
 bool list_is_empty(list_ptr l)
 {
-  return true;
+  return (l == NULL);
 }
 
 /* Search the first cel of the list & 
@@ -51,14 +95,16 @@ bool list_is_empty(list_ptr l)
  * */
 sprite_t list_head_sprite(list_ptr l)
 {
-  return NULL;
+  if (l == NULL) return NULL;
+  return l->data;
 }
 
 /* Return the next cel in list or NULL
  * */
 list_ptr list_next(list_ptr l)
 {
-  return NULL;
+  if (l == NULL) return NULL;
+  return l->next;
 }
 
 /* Search the last cel of a list 
@@ -67,13 +113,41 @@ list_ptr list_next(list_ptr l)
  * */
 sprite_t list_pop_sprite(list_ptr * l)
 {
-  return NULL;
+  if (l == NULL || *l == NULL) return NULL;
+  if ((*l)->next == NULL) {
+    sprite_t sprite = (*l)->data;
+    free(*l);
+    *l = NULL;
+    return sprite;
+  }
+  list_ptr current = *l;
+  while (current->next->next != NULL) {
+    current = current->next;
+  }
+  sprite_t sprite = current->next->data;
+  free(current->next);
+  current->next = NULL;
+  return sprite;
 }
 
 /* Remove the given cel in a list
  * */
 void list_remove(list_ptr elt, list_ptr *l)
 {
+  if (l == NULL || *l == NULL || elt == NULL) return;
+  if (*l == elt) {
+    *l = elt->next;
+    free(elt);
+    return;
+  }
+  list_ptr current = *l;
+  while (current != NULL && current->next != elt) {
+    current = current->next;
+  }
+  if (current != NULL) {
+    current->next = elt->next;
+    free(elt);
+  }
 }
 
 /* Wipe out a list. 
@@ -81,4 +155,13 @@ void list_remove(list_ptr elt, list_ptr *l)
  * */
 void list_free(list_ptr l)
 {
+  list_ptr current = l;
+  while (current != NULL) {
+    list_ptr next = current->next;
+    if (current->data != NULL) {
+      sprite_free(current->data);
+    }
+    free(current);
+    current = next;
+  }
 }
